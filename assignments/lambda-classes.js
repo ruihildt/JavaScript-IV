32c7da9718d4afefe1ee364518fd847f4530a824
ruihildt <ruihildt@protonmail.com>
2019-05-09 14:53:15 +0200

    Declare Instructor class and define demo() & grade()
    Instructor class is extended from Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,7 +1,12 @@
-// Declare Person class and define speak() metod
+// Declare Person class and define speak() method
 
 class Person {
-    constructor(name, age, location, gender) {
+    constructor({
+            name,
+            age,
+            location,
+            gender
+        }) {
         this.name = name;
         this.age = age;
         this.location = location;
@@ -12,3 +17,39 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
+
+// Declare Instructor class by extending Person
+// and define demo() and grade() methods
+class Instructor extends Person {
+    constructor({
+        name,
+        age,
+        location,
+        gender,
+        specialty,
+        favLanguage,
+        catchPhrase
+    }) {
+        super({
+            name,
+            age,
+            location,
+            gender,
+            specialty,
+            favLanguage,
+            catchPhrase
+        });
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
