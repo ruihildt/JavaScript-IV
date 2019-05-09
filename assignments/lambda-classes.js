416d039eb0b7acd352ce612659f1d4846273aa34
ruihildt <ruihildt@protonmail.com>
2019-05-09 14:33:48 +0200

    Declare Person class and define speak() method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
-// CODE here for your Lambda Classes
+// Declare Person class and define speak() metod
+
+class Person {
+    constructor(name, age, location, gender) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
